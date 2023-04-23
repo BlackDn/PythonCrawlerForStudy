a24860672e116ae9300338b9d2391a1b0f648de4
luoyang <luoyang.gao@thougthworks.com>
2023-04-23 14:33:27 +0800

    [blackdn] feat: get all articles in page 1

--- crawlText.py
+++ crawlText.py
@@ -2,20 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    singleChapterUrl = 'https://blackdn.github.io/2023/04/02/Blog-Title-Border-Tab-2022.md/'
-    response = requests.get(url=singleChapterUrl)
-    htmlContentBs = BeautifulSoup(response.text, features='html.parser')
+    def get_single_chapter_and_save(url):
+        single_chapter_url = url
+        response = requests.get(url=single_chapter_url)
+        html_content_bs = BeautifulSoup(response.text, features='html.parser')
 
-    articleContent = htmlContentBs.find(name='article')
-    referenceNode = articleContent.find(id="参考")
+        article_content = html_content_bs.find(name='article')
 
-    for node in referenceNode.find_all_next():
-        node.extract()
-    referenceNode.extract()
+        reference_node = article_content.find(class_='pager')
+        for node in reference_node.find_all_next():
+            node.extract()
+        reference_node.extract()
 
-    articleTitle = articleContent.find(name='h1').string
-    textContent = articleContent.text.strip()
+        article_title = article_content.find(name='h1').string
+        text_content = article_content.text.strip()
 
-    with open(f'{articleTitle}.txt', 'w') as file:
-        file.write(textContent)
+        with open(f'{article_title}', 'w') as file:
+            file.write(text_content)
+
+
+    base_url = 'https://blackdn.github.io'
+    get_page_response = requests.get(url=base_url)
+    page_content_bs = BeautifulSoup(get_page_response.text, features='html.parser')
+
+    chapter_list = page_content_bs.find(class_='postlist-container').find_all(class_='post-preview')
+
+    for chapter_node in chapter_list:
+        current_url = chapter_node.a['href']
+        print(f'{base_url}{current_url}')
+        get_single_chapter_and_save(f'{base_url}/{current_url}')
 
