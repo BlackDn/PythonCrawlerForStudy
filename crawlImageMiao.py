dcb9fdee014bdc264844277731a34cb9bf104962
luoyang <luoyang.gao@thougthworks.com>
2023-05-12 15:36:14 +0800

    [blackdn] feat: get page response & rename

--- crawlImageMiao.py
+++ crawlImageMiao.py
@@ -9,4 +9,8 @@ if __name__ == '__main__':
         os.mkdir(dir_path)
 
     page_number = 0
-    url_to_get_json = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={page_number}&page_size=30&biz=all&jsonp=jsonp'
+    url_to_get_page = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={page_number}&page_size=30&biz=all&jsonp=jsonp'
+
+    for current_page in range(10):
+        page_number = current_page
+        response_page = requests.get(url_to_get_page)
