f986c81f59554b57283d277f549621c3ecf5efa5
luoyang <luoyang.gao@thougthworks.com>
2023-05-12 15:29:57 +0800

    [blackdn] feat: add base url

--- crawlImageMiao.py
+++ crawlImageMiao.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
     dir_path = 'miao/'
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
+
+    page_number = 0
+    url_to_get_json = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={page_number}&page_size=30&biz=all&jsonp=jsonp'
