6a2833954df4be56f5f24ca8f7d21afc505ab4f0
luoyang <luoyang.gao@thougthworks.com>
2023-05-12 15:19:27 +0800

    [blackdn] feat: add crawlImage & make dir first

---
+++ crawlImageMiao.py
@@ -0,0 +1,9 @@
+import requests
+import os
+
+# url for BiZhiMiao space: https://space.bilibili.com/6823116/album
+if __name__ == '__main__':
+
+    dir_path = 'miao/'
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
