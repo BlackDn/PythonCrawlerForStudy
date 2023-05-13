203dbde50f58869be3bc6ad215ccce928a9fda05
blackdn <blackdn233@outlook.com>
2023-05-13 21:00:37 +0800

    [blackdn] fix: wrong code

--- crawlImageMiao.py
+++ crawlImageMiao.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     for current_page in range(page_number, last_page_number):
         url_to_get_page = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={current_page}&page_size=30&biz=all&jsonp=jsonp'
-        print(f'We are in page {page_number}')
+        print(f'We are in page {current_page}')
 
         response_page = requests.get(url_to_get_page)
         response_page_json = response_page.json()
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         # crawl all images in each page
         for item_index, img_item in enumerate(img_list):
             for pic_index, pics in enumerate(img_item['pictures']):
-                img_name = f'page{page_number}-img{item_index}-{pic_index}.jpg'
+                img_name = f'page{current_page}-img{item_index}-{pic_index}.jpg'
 
                 # skip pics when width < height
                 # if pics['img_width'] < pics['img_height']:
