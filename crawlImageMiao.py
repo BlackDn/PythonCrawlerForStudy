35d380e640ec1a81513d0b44005ae923b2aac8b6
blackdn <blackdn233@outlook.com>
2023-05-13 20:29:51 +0800

    [blackdn] fix: wrong page number in url

--- crawlImageMiao.py
+++ crawlImageMiao.py
@@ -11,10 +11,9 @@ if __name__ == '__main__':
     # crawl pages from page_number to last_page_number
     page_number = 0
     last_page_number = 10
-    url_to_get_page = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={page_number}&page_size=30&biz=all&jsonp=jsonp'
 
-    for current_page in range(last_page_number):
-        page_number = current_page
+    for current_page in range(page_number, last_page_number):
+        url_to_get_page = f'https://api.bilibili.com/x/dynamic/feed/draw/doc_list?uid=6823116&page_num={current_page}&page_size=30&biz=all&jsonp=jsonp'
         print(f'We are in page {page_number}')
 
         response_page = requests.get(url_to_get_page)
@@ -26,9 +25,10 @@ if __name__ == '__main__':
             for pic_index, pics in enumerate(img_item['pictures']):
                 img_name = f'page{page_number}-img{item_index}-{pic_index}.jpg'
 
-                if pics['img_width'] < pics['img_height']:
-                    print(f'skip {img_name}')
-                    break
+                # skip pics when width < height
+                # if pics['img_width'] < pics['img_height']:
+                #     print(f'skip {img_name}')
+                #     break
 
                 img_url = pics['img_src']
                 img_response = requests.get(img_url)
